cc974044b959343c720bf8bd1eef603b4d20a803
Piotr_Luniov <piotr_luniov@epam.com>
2019-07-29 15:09:10 +0300

    helloworld-pluniov-maven-build

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -1,7 +1,7 @@
 package com.test;
 
-import org.apache.commons.collections.Predicate;
-import org.apache.log4j.Logger;
+//import org.apache.commons.collections.Predicate;
+//import org.apache.log4j.Logger;
 
 class Project {
 
@@ -16,3 +16,4 @@ class Project {
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
+}
