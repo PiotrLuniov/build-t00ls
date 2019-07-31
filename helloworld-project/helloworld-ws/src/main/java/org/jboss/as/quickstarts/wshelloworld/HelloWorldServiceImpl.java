04e1907169526df72f3e37f905ba1e0a77936226
AliaksandrShamchonak <Aliaksandr_Shamchonak@epam.com>
2019-07-31 21:37:04 +0000

    Task6

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -32,7 +32,10 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayHello() {
-        return "Hello World!";
+        System.out.println("MNT.lab");
+	while(true){
+	}
+	return "Hello World!";
     }
 
     @Override
