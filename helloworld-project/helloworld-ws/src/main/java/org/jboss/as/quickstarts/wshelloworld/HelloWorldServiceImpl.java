6bda0362a588732d8d3cfb51ac041885c4fb78b2
vagrant <vagrant@master.local>
2019-08-01 15:58:30 +0000

    error

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -32,8 +32,9 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayHello() {
-    }    return "Hello World!";
-    
+        return "Hello World!";
+     while(true){}
+    }
    
 
     @Override
